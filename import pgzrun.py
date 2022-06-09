cae9514bf90799b03c978f279d4abd4972b8d3c6
MineralLiamG <ldg4sure@gmail.com>
2022-06-09 08:35:57 +0200

    dino actors

--- import pgzrun.py
+++ import pgzrun.py
@@ -9,6 +9,15 @@ desertback1.topleft = 0,0
 desertback2 = Actor('desertback')
 desertback2.topleft = 799,0
 
+dinolaufen1 = Actor('run1')
+dinolaufen2 = Actor('run2')
+dinolaufen3 = Actor('run3')
+dinolaufen4 = Actor('run4')
+dinolaufen5 = Actor('run5')
+dinolaufen6 = Actor('run6')
+dinolaufen7 = Actor('run7')
+dinolaufen8 = Actor('run8')
+
 def update():
     desertback1.x -=1
     desertback2.x -=1
@@ -20,18 +29,22 @@ def update():
     elif Kaktus.x <= -20:
         Kaktus.x = 818
 
+spielaktiv = True
 
 Kaktus = Actor('cactus')
 Kaktus.y = 315
 Kaktus.x = 700
+
 
 def draw():
     screen.clear()
     desertback1.draw()
     desertback2.draw()
     Kaktus.draw()
+    dinolaufen1.draw()
 
-
+while spielaktiv:
+    
 
 
 pgzrun.go()
