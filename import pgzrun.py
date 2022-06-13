9ad66b2bdea8d71f5e596bc1e1920d4835db3778
FrankfurtamA <leonard.muenchen@gmail.com>
2022-06-13 09:30:29 +0200

    neu game over

--- import pgzrun.py
+++ import pgzrun.py
@@ -1,9 +1,11 @@
-from pickle import FALSE
+from pickle import FALSE, TRUE
 import pgzrun
 import random
 
 WIDTH = 800
 HEIGHT = 400
+
+game_over = False
 
 desertback1 = Actor('desertback')
 desertback1.topleft = 0,0
@@ -11,11 +13,13 @@ desertback2 = Actor('desertback')
 desertback2.topleft = 799,0
 
 dino = Actor('idle1')
-dino.images = ['run1','run2','run3','run4','run5','run6','run7','run8']
+dino.images = ['run1','run2','run3','run4','run5','run6','run7','run8','dead8']
 dino.fps = 10
+
 
     
 def update():
+    global game_over
     desertback1.x -=1
     desertback2.x -=1
     Kaktus.x -= 1
@@ -25,17 +29,19 @@ def update():
         desertback2.x = 1199
     elif Kaktus.x <= -20:
         Kaktus.x = 818
-        gameover
+   
+    if dino.colliderect(Kaktus):
+        dino.image = "dead8"
+        game_over = TRUE
 
 Kaktus = Actor('cactus')
 Kaktus.y = 315
 Kaktus.x = 700
-dino.y = 315
+dino.y = 300
 
-def gameover():
-    if dino.colliderect(Kaktus):
-        dino.image = "dead8"
+
        
+
 
 
 def draw():
@@ -43,6 +49,11 @@ def draw():
     desertback1.draw()
     desertback2.draw()
     Kaktus.draw()
-    dino.draw()
+    if game_over:
+        screen.draw.text('Game Over', centerx=400, centery=270, color=(255,255,255), fontsize=60)
+    else:
+        dino.draw()
+        
+
 
 pgzrun.go()
